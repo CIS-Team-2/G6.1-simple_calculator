ad7bcf79c4a7ad34d397447ef356141ecd04ce87
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-05-04 19:22:33 -0700

    Add project files.

---
+++ Simple Calculator/frmSimpleCalculator.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Simple_Calculator
+{
+    partial class frmSimpleCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperandOne = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblOperandTwo = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperandOne
+            // 
+            this.lblOperandOne.AutoSize = true;
+            this.lblOperandOne.Location = new System.Drawing.Point(76, 58);
+            this.lblOperandOne.Name = "lblOperandOne";
+            this.lblOperandOne.Size = new System.Drawing.Size(101, 25);
+            this.lblOperandOne.TabIndex = 0;
+            this.lblOperandOne.Text = "Operand 1:";
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(76, 124);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(88, 25);
+            this.lblOperator.TabIndex = 0;
+            this.lblOperator.Text = "Operator:";
+            // 
+            // lblOperandTwo
+            // 
+            this.lblOperandTwo.AutoSize = true;
+            this.lblOperandTwo.Location = new System.Drawing.Point(76, 194);
+            this.lblOperandTwo.Name = "lblOperandTwo";
+            this.lblOperandTwo.Size = new System.Drawing.Size(101, 25);
+            this.lblOperandTwo.TabIndex = 0;
+            this.lblOperandTwo.Text = "Operand 2:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(76, 267);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(63, 25);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Result:";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(202, 59);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(150, 31);
+            this.txtFirst.TabIndex = 1;
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(202, 121);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(49, 31);
+            this.txtOperator.TabIndex = 2;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(202, 191);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(150, 31);
+            this.txtSecond.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(202, 261);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(150, 31);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(86, 331);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 34);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(240, 331);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 34);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmSimpleCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(429, 425);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblOperandTwo);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblOperandOne);
+            this.Name = "frmSimpleCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperandOne;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblOperandTwo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
